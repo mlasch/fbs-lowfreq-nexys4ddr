d24c443edd376a741866704c79c2fc28d243df32
marc <mlasch@mailbox.org>
2018-05-30 16:45:46 +0200

    Added external signals for transmission

--- hdl/toplevel.vhd
+++ hdl/toplevel.vhd
@@ -20,7 +20,13 @@ entity toplevel is
         aud_sd: out std_logic;
         -- in/out
         SW: in std_logic_vector(15 downto 0);
-        LED: out std_logic_vector(15 downto 0)       
+        LED: out std_logic_vector(15 downto 0);
+        
+        CA, CB, CC, CD, CE, CF, CG, DP: out std_logic;
+        AN: out std_logic_vector(7 downto 0);
+        
+        sync_in, data_in: in std_logic;
+        sync_out, data_out: out std_logic
     );
 
 end toplevel;
@@ -49,10 +55,10 @@ architecture rtl of toplevel is
     );
     end component;
    
-    signal dummy_reset: std_logic := '1';     -- dummy signal for reset
-    
-    signal golden_data: std_logic;
-    signal golden_sync: std_logic;
+    --signal dummy_reset: std_logic := '1';     -- dummy signal for reset
+
+    --signal golden_data: std_logic;
+    --signal golden_sync: std_logic;
     
     signal pout: std_logic_vector(15 downto 0);
     signal debug_out: std_logic_vector(15 downto 0);
@@ -70,19 +76,30 @@ architecture rtl of toplevel is
         rst_n => SW(15), --dummy_reset,
         vaux_p => vauxp3,
         vaux_n => vauxn3,
-        s_out => golden_data,
-        s_sync => golden_sync
+        s_out => data_out,
+        s_sync => sync_out
     );
     
     sink_0: signal_sink
     port map(
         clk_in => CLK100MHZ,
         rst_n => SW(15), --dummy_reset,
-        s_in => golden_data,
-        s_sync => golden_sync,
+        s_in => data_in,
+        s_sync => sync_in,
         sw => SW(2 downto 0),
         pwm_out => aud_pwm,
         debug_out => debug_out
     );
-
+    
+    -- force the 7-segment lines to GND
+    CA <= '0';
+    CB <= '0';
+    CC <= '0';
+    CD <= '0';
+    CE <= '0';
+    CF <= '0';
+    CG <= '0';
+    DP <= '0';
+    AN <= 8x"ff";
+     
 end rtl;
